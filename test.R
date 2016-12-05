d7a8a5b54cf6b21a925e86aaf268be375f473003
myellen <max.yellen@gmail.com>
2016-12-05 16:37:57 -0500

    testing stuff

--- test.R
+++ test.R
@@ -1,5 +1,7 @@
 #install.packages("devtools")
-#library(glmnet)
+if(!"devtools" %in% rownames(installed.packages())) { install.packages("devtools", dependencies = TRUE) }
+install.packages("glmnet")
+library(glmnet)
 #library(devtools)
 #devtools::use_travis()
 
@@ -7,3 +9,5 @@ f1 <- function(x, y) {
   x+y
 }
 f1(1,2)
+
+data <- read.csv("mf850-finalproject-data.csv")
