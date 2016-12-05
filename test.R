74294b6a431d0a9653fd34e281460d4629f1181b
myellen <max.yellen@gmail.com>
2016-12-05 17:03:44 -0500

    made function for intalling packages if needed and loading them

--- test.R
+++ test.R
@@ -1,9 +1,23 @@
 #install.packages("devtools")
-if(!"devtools" %in% rownames(installed.packages())) { install.packages("devtools", dependencies = TRUE) }
-install.packages("glmnet")
-library(glmnet)
 #library(devtools)
 #devtools::use_travis()
+
+loadpackages <- function(packagestoinstall) {
+  packagestoinstall_split <- strsplit(packagestoinstall,", ")[[1]]
+  for (packagename in packagestoinstall_split) {
+    if(!"devtools" %in% rownames(installed.packages())){
+      install.packages("devtools", dependencies = TRUE)
+    }
+    else {
+      print(paste(packagename,"already installed"))
+    }
+    library(packagename, character.only=TRUE)
+  }
+}
+
+requiredpackages <- "devtools, glmnet"
+loadpackages(requiredpackages)
+
 
 f1 <- function(x, y) {
   x+y
