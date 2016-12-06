0db388c10735c5fb6b54a546d3577348ddb44b03
Malcolm Taylor <malctaylor15@gmail.com>
2016-12-06 09:42:02 -0500

    Plotting
    Added some plots for the response variable
    
    - Helps understand the data A LOT
    - notice the high levels of auto correlation

--- data_exploration-12-5.R
+++ data_exploration-12-5.R
@@ -4,35 +4,52 @@ data <- read.csv("mf850-finalproject-data.csv")
 summary(data)
 # Seperate the response variable 
 REMONTH <- data$RETMONTH_SPX
-# Look at quantiles for response
-summary(REMONTH)
 # Histogram for response 
 hist(REMONTH, breaks =60)
+# How many unique response variables 
+length(unique(REMONTH))
 # Count how many returns are higher and lower 
 (re_up <- length(REMONTH[REMONTH<0]))
 (re_down <- length(REMONTH[REMONTH>0])) 
-# How many unique response varible 
-length(unique(REMONTH))
-
+# Up to down ratio 
+re_up/re_down
+# Stop up down as new column vector 
 up_down <- ifelse(REMONTH >0, 1, 0)
 
+# Put plots above and below 
+par(mfrow = 2:1)
+# Plot the last 100 points 
+plot(REMONTH[(length(REMONTH)-109):length(REMONTH)], type = 'l')
+# Plot the first 100 points 
+plot(REMONTH[0:100], type = 'l')
+
+# Ontop of each other 
+par(mfrow = c(1,1))
+plot(REMONTH[(length(REMONTH)-109):length(REMONTH)], type = 'l', col= 'red', lwd = 3)
+lines(REMONTH[0:109], type = 'l', col= 'blue', lwd = 2)
+
+length(unique(REMONTH[(length(REMONTH)-100):length(REMONTH)]))
+acf(REMONTH)
+
+# Look at first 50 points of high low response 
+plot(up_down[1:50], type = 'l')
+
+# Take out industry, date, retmonth variables 
+industry <- data$Industry
+date <- data$Date
+data_no_ind <- data
+data_no_ind$Industry <- NULL
+data_no_ind$Date <- NULL
+# Scale data 
+data_no_ind <- scale(data_no_ind)
+
+# PCA 
+pca <- prcomp(data_no_ind, center = TRUE, scale = FALSE)
+summary(pca)
+plot(pca)
+# Maybe take the first 32 variables- they explain 94% of the variance and the increase becomes less than 1% after that 
+# Will loose a lot of interpretability 
 
 
-# Attempt blind logistic regression 
-data1 <- data
-data1$Date <- NULL
-data1$Industry <-NULL
-x <- data.frame(model.matrix(RETMONTH_SPX~., data= data1))
-data2 <-data.frame(data1)
-data2$RETMONTH_SPX <- NULL
 
-data2 <- c(up_down,data2)
-fit2 <- glm(up_down ~., data = data2, family = "binomial")
-
-
-
-y <- as.factor(data1$RETMONTH_SPX)
-library(nnet)  # multinom 
-# Multinomial regression 
-fit1 <- multinom(RETMONTH_SPX ~. , data= data1)
 
