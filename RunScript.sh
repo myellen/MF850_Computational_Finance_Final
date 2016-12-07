a5cf482443e232bdd2f6609e12919ad83201af6d
myellen <max.yellen@gmail.com>
2016-12-07 00:05:10 -0500

    trying out different scripts

--- RunScript.sh
+++ RunScript.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-#Rscript -e 'source("test.R")'
+Rscript -e "source(\"test.R\", echo=TRUE)"
 #Rscript -e "f1(1,2)"
 
 #Rscript "test.R"
-Rscript "cm_v1.R"
+Rscript --verbose "cm_v1.R"
 Rscript "data_exploration-12-5.R"
 Rscript "GPUAcceleratedClustering.R"
 Rscript "NeuralNet.R"
