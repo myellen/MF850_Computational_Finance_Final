811da4a76ef7852a0e56247ba4ae9940cd40ffa9
myellen <max.yellen@gmail.com>
2016-12-07 01:26:58 -0500

    Testtools (#4) (#5)
    * trying out different scripts
    
    * add lintr
    
    * source all files dynamically
    
    * RunScript for each file separately
    
    * Create README.md
    
    * add other scripts to testing. try changing order
    
    * Add everyone back in before merge to development

--- RunScript.sh
+++ RunScript.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -e
 
-#Rscript -e 'source("test.R")'
-#Rscript -e "f1(1,2)"
+exitstatus=0
 
-#Rscript "test.R"
-Rscript "cm_v1.R"
-Rscript "data_exploration-12-5.R"
-Rscript "GPUAcceleratedClustering.R"
-Rscript "NeuralNet.R"
+for file in "$@"
+do
+    Rscript -e "source(\"$file\", echo=TRUE)"
+done
 
+exit $exitstatus
