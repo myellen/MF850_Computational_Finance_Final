bd5850104ae080f766e4fdc4b338d218dae3dac6
myellen <max.yellen@gmail.com>
2016-12-06 21:36:07 -0500

    continuous intgration for new scripts

--- RunScript.sh
+++ RunScript.sh
@@ -4,4 +4,8 @@ set -e
 #Rscript -e 'source("test.R")'
 #Rscript -e "f1(1,2)"
 
-Rscript "test.R"
+#Rscript "test.R"
+Rscript "cm_v1.R"
+Rscript "data_exploration-12-5.R"
+Rscript "GPUAcceleratedClustering.R"
+Rscript "NeuralNet.R"
