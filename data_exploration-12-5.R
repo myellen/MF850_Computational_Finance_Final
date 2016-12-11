93ad7ce72385f74b66bee7fdd859438420a650b1
malctaylor15 <malctaylor15@gmail.com>
2016-12-10 23:12:46 -0500

    Lintr changes Added and erased some spaces to make Lintr happier Hopefully fewer than 4 emails this time...

--- data_exploration-12-5.R
+++ data_exploration-12-5.R
@@ -3,17 +3,17 @@ install.packages("boot")
 # Input the data
 data <- read.csv("mf850-finalproject-data.csv")
 summary(data)
-# Seperate the response variable 
+# Seperate the response variable
 RETMONTH <- data$RETMONTH
-# Histogram for response 
-hist(RETMONTH, breaks =60)
-# Count how many returns are higher and lower 
-(re_up <- length(RETMONTH[RETMONTH<0]))
-(re_down <- length(RETMONTH[RETMONTH>0])) 
-# Percentage of increases - Baseline   
-re_up/(re_up+re_down)
+# Histogram for response
+hist(RETMONTH, breaks = 60)
+# Count how many returns are higher and lower
+(re_up <- length(RETMONTH[RETMONTH < 0]))
+(re_down <- length(RETMONTH[RETMONTH > 0]))
+# Percentage of increases - Baseline
+re_up / (re_up + re_down)
 # Stop up down as new column vector 
-up_down <- ifelse(RETMONTH >0, 1, 0)
+up_down <- ifelse(RETMONTH > 0, 1, 0)
 
 # Determine how many unique predictors there are  
 # Function to measure number of unique elements in column 
@@ -23,33 +23,33 @@ numb_unique <- function(x) {
 }
 
 # Apply number of unique function to all columns of the data set 
-uniqueness <- apply(data,2,numb_unique)
+uniqueness <- apply(data, 2, numb_unique)
 # Count how many columns have less than 50 unique values (indicating they are categorical)
-length(uniqueness[uniqueness<50])
-# Determine which variables should be categorical variables 
-which(uniqueness <50)
-# Already saw Industry has levels 
+length(uniqueness[uniqueness < 50])
+# Determine which variables should be categorical variables
+which(uniqueness < 50)
+# Already saw Industry has levels
 
 # Establish plotting points - Last 1000 data points 
 n <- length(RETMONTH)
-n_low <- n-1000 
+n_low <- n - 1000
 
 # Plot last __ data points 
-plot(RETMONTH[n_low:n], type = 'l', col= 'red', lwd = 2)
-abline(h= mean(RETMONTH[n_low:n]), col = "blue", lwd = 2)
+plot(RETMONTH[n_low:n], type = "l", col = "red", lwd = 2)
+abline(h = mean(RETMONTH[n_low:n]), col = "blue", lwd = 2)
 # A priori MSE - squared error from the mean 
-MSE_i <- (RETMONTH[n_low:n]-mean(RETMONTH[n_low:n]))^2
+MSE_i <- (RETMONTH[n_low:n] - mean(RETMONTH[n_low:n])) ^ 2
 # MSE
 (MSE <- mean(MSE_i))
 
-# Take out industry, date, retmonth variables 
+# Take out industry, date, retmonth variables
 industry <- data$Industry
 date <- data$Date
 data_no_ind <- data
 data_no_ind$Industry <- NULL
 data_no_ind$Date <- NULL
 data_no_ind$RETMONTH <- NULL
-# Scale data 
+# Scale data
 data_no_ind <- scale(data_no_ind)
 
 # PCA 
@@ -60,13 +60,13 @@ plot(pca)
 # Will loose a lot of interpretability 
 
 # Save first 9 principal components, last __ data points  ~ 58% variance, decrease <2% 
-xs <- pca$x[n:n_low,1:9]
+xs <- pca$x[n:n_low, 1:9]
 xs_df <- cbind(RETMONTH[n_low:n], as.data.frame(xs))
 # pairs(xs_df)
 # not much there 
 
 # Get the data from the new data frame for the specified length 
-data2 <- data_no_ind[n_low:n,]
+data2 <- data_no_ind[n_low:n, ]
 # Combine categorical output with predictors 
 data3 <- cbind(up_down[n_low:n], data2)
 # Change data type to data frame -- preparations for logistic regression 
@@ -74,11 +74,11 @@ data3 <- data.frame(data3)
 # Rename repsonse column in data frame  
 names(data3)[1] <- "updown"
 # Fit logistic regression 
-fit <- glm(updown~., data= data3, family = "binomial")
+fit <- glm(updown~., data = data3, family = "binomial")
 # Find accuracy of logistic regression 
 library(boot) #cv.glm
 # In sample accuracy 
-cv_est <- cv.glm(data3, fit, K= 10)$delta[1]
+cv_est <- cv.glm(data3, fit, K = 10)$delta[1]
 # In sample accuracy for logistic regression 
-1-cv_est
+1 - cv_est
 
--- test_train_split.R
+++ test_train_split.R
@@ -5,11 +5,10 @@
 data <- read.csv("mf850-finalproject-data.csv")
 y <- data$RETMONTH
 # Peak at data again 
-# summary(data)
 
 # Determine test and train size 
-test_size <- 300 
-train_size <- 2000 
+test_size <- 300
+train_size <- 2000
 
 # Store  test and train response variable 
 
@@ -17,48 +16,48 @@ train_size <- 2000
 # Training set is next train_size after test_size 
 
 test_high <- nrow(data)
-test_low <- test_high - test_size # Lower row number for test set 
-train_high <- test_low - 1 # Upper row number for training set 
-train_low <- train_high - train_size # Lower row number for training set  
+test_low <- test_high - test_size # Lower row number for test set
+train_high <- test_low - 1 # Upper row number for training set
+train_low <- train_high - train_size # Lower row number for training set
 
-# Subset the Response variable into test and training sets 
+# Subset the Response variable into test and training sets
 y_test <- y[test_low:test_high]
 y_train <- y[train_low:train_high]
 
 # Remove Date, Industry, Returns (response variables)
 data$Date <- NULL
-data$Industry <- NULL 
-data$RETMONTH <- NULL 
+data$Industry <- NULL
+data$RETMONTH <- NULL
 
 # Create training set 
 data <- scale(data)
-x_test <- data[test_low:test_high,]
+x_test <- data[test_low:test_high, ]
 x_train <- data[train_low:train_high, ]
 
 # Some benchmarks for regression and categorical analysis 
 
 # Baseline MSE from guessing the mean for in sample accuracy 
-MSE_train <- mean((y_train - mean(y_train))^2)
+MSE_train <- mean( (y_train - mean(y_train)) ^ 2)
 # Baseline MSE from guessing the mean for test (out of sample) accuracy  
-MSE_test <- mean((y_test - mean(y_test))^2)
+MSE_test <- mean( (y_test - mean(y_test)) ^ 2)
 
 
 # Train set for categorical analysis 
-y_train_cat <- ifelse(y_train >0, 1, 0)
+y_train_cat <- ifelse(y_train > 0, 1, 0)
 # Ratio of high vs low returns for TRAIN set 
-(high_low_ratio <- sum(y_train_cat == 1)/length(y_train_cat))
+(high_low_ratio <- sum(y_train_cat == 1)  / length(y_train_cat))
 
 # Test set for categorical analysis 
-y_test_cat <- ifelse(y_test >0, 1, 0)
+y_test_cat <- ifelse(y_test > 0, 1, 0)
 # Ratio of high vs low returns for TEST set 
-(high_low_ratio <- sum(y_test_cat == 1)/length(y_test_cat))
+(high_low_ratio <- sum(y_test_cat == 1) / length(y_test_cat))
 # A priori number to beat 
 
 
 
 # Fun Comparision/ Sampling stuff - want to make sure we have a representative sample
 # Plot histogram of full, test and training for response
-par(mfrow=c(1,3))
+par(mfrow = c(1, 3))
 hist(y, breaks = 40)
 hist(y_train, breaks = 40)
 hist(y_test, breaks = 40)
@@ -67,19 +66,16 @@ hist(y_test, breaks = 40)
 # Look at means for the samples 
 c(mean(y), mean(y_train), mean(y_test))
 
-par(mfrow= c(1,1))
+par(mfrow = c(1, 1))
 # 
 qqplot(y, y_train)
 qqline(y_train)
 ks.test(y_train, y)
 #
-qqplot(y,y_test)
+qqplot(y, y_test)
 qqline(y_test)
 ks.test(y_test, y)
 # 
 qqplot(y_test, y_train)
 qqline(y_test)
 ks.test(y_test, y_train)
-
-
-
