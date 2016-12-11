967da0208c83dcc11b3fdcdca85dcc89b5a490c8
malctaylor15 <malctaylor15@gmail.com>
2016-12-10 23:28:52 -0500

    Lintr More Lintr changes Fixed travis file

--- random_forest_cat.R
+++ random_forest_cat.R
@@ -1,43 +1,43 @@
-# Random forest for classification 
+# Random forest for classification
 
-# Template for importing data into train and test sets 
-# Also need to remove some variables 
+# Template for importing data into train and test sets
+# Also need to remove some variables
 
 # Import data
 data <- read.csv("mf850-finalproject-data.csv")
 y <- data$RETMONTH
-# Peak at data again 
+# Peak at data again
 # summary(data)
 
-# Determine test and train size 
-test_size <- 300 
-train_size <- 2000 
+# Determine test and train size
+test_size <- 300
+train_size <- 2000
 
-# Store  test and train response variable 
+# Store  test and train response variable
 
 # Test set is last test_size
-# Training set is next train_size after test_size 
+# Training set is next train_size after test_size
 
 test_high <- nrow(data)
-test_low <- test_high - test_size # Lower row number for test set 
-train_high <- test_low - 1 # Upper row number for training set 
-train_low <- train_high - train_size # Lower row number for training set  
+test_low <- test_high - test_size # Lower row number for test set
+train_high <- test_low - 1 # Upper row number for training set
+train_low <- train_high - train_size # Lower row number for training set
 
-# Subset the Response variable into test and training sets 
+# Subset the Response variable into test and training sets
 y_test <- y[test_low:test_high]
 y_train <- y[train_low:train_high]
 
 # Remove Date, Industry, Returns (response variables)
 data$Date <- NULL
-data$Industry <- NULL 
-data$RETMONTH <- NULL 
+data$Industry <- NULL
+data$RETMONTH <- NULL
 
-# Create training set 
+# Create training set
 data <- scale(data)
-x_test <- data[test_low : test_high,]
+x_test <- data[test_low : test_high, ]
 x_train <- data[train_low : train_high, ]
 
-# Train set for categorical analysis 
+# Train set for categorical analysis
 y_train_cat <- as.factor(ifelse(y_train > 0, 1, 0))
 # Ratio of high vs low returns for TRAIN set 
 (high_low_ratio_train <- sum(y_train_cat == 1) / length(y_train_cat))
@@ -61,14 +61,14 @@ fit_rf1 <- randomForest(x_train, y_train_cat, xtest = x_test, ytest = y_test_cat
 (rf_confusion_train <- fit_rf1$confusion)
 # In sample accuracy - sum the 1st and 4th element of matrix (6 elements total) 
 # Divide by the total number of element (sum 1st through 4th elements)
-sum(rf_confusion_train[c(1,4)]) / sum(rf_confusion_train[1:4])
+sum(rf_confusion_train[c(1, 4)]) / sum(rf_confusion_train[1:4])
 # Compare with in sample a priori estimate
 high_low_ratio_train
 
 # Out of sample accuracy 
 (rf_confusion_test <- fit_rf1$test$confusion)
 # Out of sample accuracy 
-sum(rf_confusion_test[c(1,4)]) / sum(rf_confusion_test[1:4])
+sum(rf_confusion_test[c(1, 4)]) / sum(rf_confusion_test[1:4])
 # Compare to a priori guess 
 high_low_ratio_test
 
--- random_forest_reg.R
+++ random_forest_reg.R
@@ -4,21 +4,19 @@
 # Import data
 data <- read.csv("mf850-finalproject-data.csv")
 y <- data$RETMONTH
-# Peak at data again 
-# summary(data)
 
-# Determine test and train size 
-test_size <- 300 
-train_size <- 2000 
+# Determine test and train size
+test_size <- 300
+train_size <- 2000
 
-# Store  test and train response variable 
+# Store  test and train response variable
 
 # Test set is last test_size
-# Training set is next train_size after test_size 
+# Training set is next train_size after test_size
 
 test_high <- nrow(data)
-test_low <- test_high - test_size # Lower row number for test set 
-train_high <- test_low - 1 # Upper row number for training set 
+test_low <- test_high - test_size # Lower row number for test set
+train_high <- test_low - 1 # Upper row number for training set
 train_low <- train_high - train_size # Lower row number for training set  
 
 # Subset the Response variable into test and training sets 
@@ -30,18 +28,17 @@ data$Date <- NULL
 data$Industry <- NULL 
 data$RETMONTH <- NULL 
 
-# Create training set 
+# Create training set
 data <- scale(data)
-x_test <- data[test_low:test_high,]
+x_test <- data[test_low:test_high, ]
 x_train <- data[train_low:train_high, ]
 
 # Some benchmarks for regression and categorical analysis 
 
 # Baseline MSE from guessing the mean for in sample accuracy 
-MSE_train <- mean((y_train - mean(y_train))^2)
+MSE_train <- mean((y_train - mean(y_train)) ^ 2)
 # Baseline MSE from guessing the mean for test (out of sample) accuracy  
-MSE_test <- mean((y_test - mean(y_test))^2)
-
+MSE_test <- mean((y_test - mean(y_test)) ^ 2)
 
 
 ############################################
@@ -54,19 +51,17 @@ fit_rf2 <- randomForest(x_train, y_train, xtest = x_test, ytest = y_test, ntree 
 
 
 names(fit_rf2)
-# Calculate in sample MSE 
-(MSE_train_rf <- mean((fit_rf2$predicted - y_train)^2))
+# Calculate in sample MSE
+(MSE_train_rf <- mean((fit_rf2$predicted - y_train) ^ 2))
 # Compared to baseline in sample MSE
 MSE_train
-# Percentage increase 
-((abs(MSE_train_rf - MSE_train))/MSE_train)*100
+# Percentage increase
+((abs(MSE_train_rf - MSE_train)) / MSE_train) * 100
 
-# Out of sample MSE 
-(MSE_rf <- mean((fit_rf2$test$predicted - y_test)^2))
-# Compared to baseline 
+# Out of sample MSE
+(MSE_rf <- mean((fit_rf2$test$predicted - y_test) ^ 2))
+# Compared to baseline
 MSE_test
 
 # Very small gain percentage gain over regular 
-(abs(MSE_rf - MSE_test)/MSE_test)*100
-
-
+(abs(MSE_rf - MSE_test) / MSE_test) * 100
