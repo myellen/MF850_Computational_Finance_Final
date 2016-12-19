1804a60ec3c89df81da3c11b2c9a31910b7256fb
malctaylor15 <malctaylor15@gmail.com>
2016-12-19 00:09:39 -0500

    Fixing various RDF compatabilities and ensuring the final R script works
    New data files for testing

--- IG-CM-MT-MY_mf850finalproject_iii.R
+++ IG-CM-MT-MY_mf850finalproject_iii.R
@@ -19,24 +19,26 @@ removeLastMonth <- function(stockdata) {
 source("MF850Utilities.R") # use this for now
 
 # assume csv like mf850-finalproject-data.csv with RETMONTH removed
-df <- read.csv("mf850-finalproject-data-NORETMONTH.csv")
-df <- removeLastMonth(df)
-df <- na.omit(df)
-
-# Scale data - remove categorical variable and add it back 
-Industry <- df$Industry
-df <- within(df, rm("Industry"))
-df <- scale(df)
-df$Industry <- Industry
+df <- read.csv("TestDF_no_returns.csv")
+y_test <- read.csv("TestDF_returns.csv")
+# Remove column with numbers 
+df <- within(df, rm("X"))
+y_train <- within(y_test, rm("X"))
+# Switch y_train to be categorical
+y_train <- as.factor(ifelse(y_train > 0, 1, 0))
 
 # remove columns we don't use
 df$Date <- NULL
 df$compid <- NULL
 df$RETMONTH_SPX <- NULL
 
+# Scale data - remove categorical variable and add it back 
+Industry <- df$Industry
+df <- within(df, rm("Industry"))
+df <- scale(df)
+df <- as.data.frame(df)
+df$Industry <- as.factor(Industry)
 
-originalLag <- loadLaggedDataSet()
-y_test <- originalLag$futurereturns
 
 ###################
 ### GLMNET - LOGISITIC 
@@ -49,10 +51,13 @@ bestLambda <- readRDS(LogisticRegressionModelLambdaFile)
 # Reformat data for glmnet 
 x_data_lin <- model.matrix(~., data = df)
 # Predict using optimal Linear Regression Coefficients 
-predict_glm <- predict.glmnet(glm_best, newx = x_data, type = "response", s = bestLambda)
-
-# Calculate Glm Model MSE 
-(MSE_glm <- mean((predict_glm - y_train)^2))
+predict_glm <- predict.glmnet(glm_best, newx = x_data_lin, type = "response", s = bestLambda)
+# Change log odds to categorical variables 
+predict_glm <- ifelse(predict_glm > 0, 1, 0)
+# Ensure data types are the same int -> factors 
+predict_glm <- as.factor(predict_glm)
+# Calculate Glm Model error rate 
+(GLM_error_rate <- mean((predict_glm != y_train)))
 
 
 ##################
@@ -66,8 +71,8 @@ rf_cat_model <- readRDS(RandomForestCatFile)
 x_data_rf <- within(df, rm("Industry"))
 
 # Predict using Random Forest 
-predict_rf <- predict(rf_cat_mode, newdata = x_data_rf)
+predict_rf <- predict(rf_cat_model, newdata = x_data_rf)
 
-# Calculate Random Forest MSE 
-(MSE_rf <- mean((predict_rf - y_train)^2))
+# Calculate Random Forest Merror rate 
+(RF_error_rate <- mean((predict_rf == y_train)))
 
